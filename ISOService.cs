e06066479a23645fa07a8f5d9a1f5ab9ebb46d4f
cvl1rt <135095030+cvl1rt@users.noreply.github.com>
2025-02-23 18:26:31 +0800

    Add initial solution
    Add initial solution

---
+++ ISOService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIRToISO
+{
+    internal static class ISOService
+    {
+        public static bool IsEmpty()
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConfigurationManager.ConnectionStrings["ISO"].ConnectionString))
+            {
+                sqlCon.Open();
+                using (SqlCommand sqlCmd = sqlCon.CreateCommand())
+                {
+                    sqlCmd.CommandText = @"SELECT COUNT(1) FROM File_Upload_Log WHERE Check_Flag = 0";
+
+                    return Convert.ToInt32(sqlCmd.ExecuteScalar()) == 0;
+                }
+            }
+        }
+    }
+}
---
+++ ProgramService.cs
@@ -0,0 +1,50 @@
+﻿using DIRToISO.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIRToISO
+{
+    internal static class ProgramService
+    {
+        public static int Count { get; set; } = 1;
+        public static void Run()
+        {
+            string programPath = ConfigurationManager.AppSettings["ProgramPath"];
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string url = programPath;
+                    HttpResponseMessage response = client.GetAsync(url).Result;
+
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine(responseBody);
+                }
+                catch (HttpRequestException ex)
+                {
+                    new Log() { 
+                        Message = ex.ToString(),
+                        LogTime = DateTime.Now,
+                        Times = Count
+                    }.Write();
+                }
+            }
+            Count++;
+
+            if (!ISOService.IsEmpty())
+            {
+                System.Console.WriteLine($"times: {Count}");
+                Run();
+            }
+        }
+    }
+}
